a53f1da6fdcd2feb77a360731d74227bc8b8cdb7
Erik Anderson <erikanderson.lm@gmail.com>
2023-10-06 22:10:14 -0300

    add toast library

--- src/pages/Filmes/index.js
+++ src/pages/Filmes/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate, json } from "react-router-dom";
 import './filme-info.css';
+import { toast } from 'react-toastify';
 
 import api from '../../services/api'
 
@@ -48,13 +49,13 @@ function Filme() {
         const  temFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
 
         if(temFilme){
-            alert("ESSE FILME JA ESTÁ NA LISTA");
+            toast.warn("Este filme ja está na sua lista!")
             return;
         }
 
         filmesSalvos.push(filme);
         localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
-        alert("FILME SALVO COM SUCESSO!")
+        toast.success("Filme salvo com sucesso!")
 
     }
 
